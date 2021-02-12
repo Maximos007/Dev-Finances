cab7ac7b5cd059b9442806b829bd5965955c9d2c
Maximos007 <waslleymax@gmail.com>
2021-02-12 19:16:50 -0300

    Update scripts.js

--- Finances/scripts.js
+++ Finances/scripts.js
@@ -53,11 +53,13 @@ const DOM = {
   
   },
   innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
 
+    const amount = Utils.formatCurrency(transaction.amount)
 
     const html = `
       <td class="description">${transaction.description}</td>
-      <td class="expense">${transaction.amount}</td>
+      <td class="${CSSclass}">${transaction.amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
         <img src="./assets/minus.svg" alt="Remover Transação" >
@@ -68,6 +70,18 @@ const DOM = {
   }
 }
 
-DOM.addTransaction(transactions[0])
-DOM.addTransaction(transactions[1])
-DOM.addTransaction(transactions[2])
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : ''
+
+    value = String(value).replace(/\D/g, '')
+
+    value = Number(value) / 100
+
+    console.log(value)
+  }
+}
+
+transactions.forEach(function(transaction) {
+  DOM.addTransaction(transaction)
+})
